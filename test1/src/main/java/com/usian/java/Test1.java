3f876bcc097795afdce4b68515c1ac1337c76f1a
wangwenhui6 <1303051149@qq.com>
2020-05-13 15:02:00 +0800

    修改Test1

--- test1/src/main/java/com/usian/java/Test1.java
+++ test1/src/main/java/com/usian/java/Test1.java
@@ -1,7 +1,8 @@
 package com.usian.java;
 
+//怎么回事
 public class Test1 {
     public static void main(String[] args) {
-        System.out.println("java Test1 Git...");
+        System.out.println("java Test1 Git...欧里给");
     }
 }
